cd5cb77c8d68c8dd24189ebb088a13068441a43c
Priyanshi <Priyanshi.42213@stu.upes.ac.in>
2026-09-03 12:15:51 +0530

    Area of rectangle

---
+++ day2a.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+int length =5;
+int breadth =10;
+printf("The area of rectangle is %d" , length * breadth);
+printf("The perimeter of rectangle is %d" , 2*(length+breadth));
+return 0;
+}
