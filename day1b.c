4587f97088b52c622047e14b32fb6d392860c34e
Priyanshi <Priyanshi.42213@stu.upes.ac.in>
2026-09-03 09:11:51 +0530

    Input of two numbers

---
+++ day1b.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+int a=10;
+int b=2;
+printf("The sum of a and b is %d",a+b);
+printf("The difference of a and b is %d",a-b);
+printf("The product of a and b is %d",a*b);
+printf("The quotient is %d",a%b);
+return 0;
+}
