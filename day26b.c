60aee36b8ce94efdfaae5d58fbb7c92ebaefea1e
Priyanshi <Priyanshi.42213@stu.upes.ac.in>
2026-09-10 10:36:38 +0530

    Added program

---
+++ day26b.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int stars[] = {1, 3, 5, 3, 1};
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < stars[i]; j++) {
+            printf("*\n");
+        }
+
+        if (i < 4) {
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
