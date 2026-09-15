70e7831c890d7dd58443db98d96d18a578852548
Priyanshi <Priyanshi.42213@stu.upes.ac.in>
2026-09-15 23:32:39 +0530

    Added program

---
+++ day32a.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n, m, i;
+
+    printf("Enter size of first array: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    printf("Enter elements of first array: ");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter size of second array: ");
+    scanf("%d", &m);
+
+    int b[m];
+
+    printf("Enter elements of second array: ");
+    for(i = 0; i < m; i++) {
+        scanf("%d", &b[i]);
+    }
+
+    printf("Merged array: ");
+
+    for(i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    for(i = 0; i < m; i++) {
+        printf("%d ", b[i]);
+    }
+
+    return 0;
+}
