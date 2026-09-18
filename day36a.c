ee8793569cad83267567cecebf13aaf219e23313
Priyanshi <Priyanshi.42213@stu.upes.ac.in>
2026-09-18 20:58:20 +0530

    Added program

---
+++ day36a.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[10][10], rows, cols;
+    
+    scanf("%d %d", &rows, &cols);
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
