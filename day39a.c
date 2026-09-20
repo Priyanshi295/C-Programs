7828bda11a2386412fddc01f710f45b95d857460
Priyanshi <Priyanshi.42213@stu.upes.ac.in>
2026-09-20 22:07:31 +0530

    Added program

---
+++ day39a.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, distinct = 1;
+
+    scanf("%d", &n);
+
+    int a[n][n];
+
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            scanf("%d", &a[i][j]);
+
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (a[i][i] == a[j][j]) {
+                distinct = 0;
+                break;
+            }
+        }
+    }
+
+    if (distinct)
+        printf("True");
+    else
+        printf("False");
+
+    return 0;
+}
